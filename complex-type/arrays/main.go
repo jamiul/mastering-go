b1f171ce321ea8d304bf0a06ac04952fde8e3b5c
jamiul <jamiulewu24@gmail.com>
2025-02-07 23:46:13 +0600

    Array slices

--- complex-type/arrays/main.go
+++ complex-type/arrays/main.go
@@ -1,18 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main()  {
 	// Array declaration
-	var colors [3]string
-	colors[0] = "Red"
-	colors[1] = "Green"
-	colors[2] = "Blue"
+	// var colors [3]string
+	// colors[0] = "Red"
+	// colors[1] = "Green"
+	// colors[2] = "Blue"
 
 	// Array declaration
-	var numbers = [5]int{1, 2, 3, 4, 5}
-	fmt.Println(numbers)
+	// var numbers = [5]int{1, 2, 3, 4, 5}
+	// fmt.Println(numbers)
 
-	fmt.Println("Number of colors:", len(colors))
-	fmt.Println("Number of numbers:", len(numbers))
+	// fmt.Println("Number of colors:", len(colors))
+	// fmt.Println("Number of numbers:", len(numbers))
+
+	// Managed ordered value in slice
+	// This is a slice
+	var colors = make([]string, 0, 3)
+	colors = append(colors, "Red", "Green", "Blue")
+	fmt.Println(colors)
+	colors = append(colors, "Purple", "Yellow")
+	fmt.Println(colors)
+
+	colors = remove(colors, 1)
+	fmt.Println(colors)
+
+	sort.Strings(colors)
+	fmt.Println(colors)
+}
+
+func remove(slice []string, i int) []string {
+	return append(slice[:i], slice[i+1:]...)
 }
