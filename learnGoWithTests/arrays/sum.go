1a5c8a76ae0a47144e8429d426c84c85af2dfe5e
jamiul <jamiulewu24@gmail.com>
2025-07-21 18:47:55 +0600

    user append to test arrays

--- learnGoWithTests/arrays/sum.go
+++ learnGoWithTests/arrays/sum.go
@@ -9,11 +9,9 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lenghtOfNumbers := len(numbersToSum)
-	sums := make([]int, lenghtOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 	return sums
 }
