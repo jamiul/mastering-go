bbc1a92e06bd7713bd0e8d07b5c3ff16e8b89854
jamiul <jamiulewu24@gmail.com>
2025-07-21 18:43:51 +0600

    test with arrays slices

--- learnGoWithTests/arrays/sum.go
+++ learnGoWithTests/arrays/sum.go
@@ -7,3 +7,13 @@ func Sum(numbers []int) int {
 	}
 	return sum;
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lenghtOfNumbers := len(numbersToSum)
+	sums := make([]int, lenghtOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
