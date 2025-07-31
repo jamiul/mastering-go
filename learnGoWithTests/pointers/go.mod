711feb34e7c7885e138c6adab49b9d8707e1fe43
jamiul <jamiulewu24@gmail.com>
2025-08-01 00:44:17 +0600

    First wallet test with pointer

---
+++ learnGoWithTests/pointers/go.mod
@@ -0,0 +1,3 @@
+module github.com/alam/LearnGoWithTests/pointers
+
+go 1.24.4
---
+++ learnGoWithTests/pointers/wallet.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in deposit is %p\n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
