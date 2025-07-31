44e8054de6cf2a3432615793cf91bdd136469e72
jamiul <jamiulewu24@gmail.com>
2025-08-01 05:47:00 +0600

    refactor the code for pointers test

--- learnGoWithTests/pointers/wallet.go
+++ learnGoWithTests/pointers/wallet.go
@@ -24,10 +24,11 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
 
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.balance {
-		return errors.New("oh no, not enough funds")
+		return ErrInsufficientFunds
 	}
 
 	w.balance -= amount
